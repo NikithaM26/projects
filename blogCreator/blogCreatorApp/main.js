a5013ef7bb0b373eafadf3fe2a915dbb68a606bd
NIKITHA. M <61590715+NikithaM26@users.noreply.github.com>
2021-09-23 20:32:50 +0530

    Delete blogCreator/blogCreatorApp directory

--- blogCreator/blogCreatorApp/main.js
+++
@@ -1,8 +0,0 @@
-function wordCount() {
-  var blog = document.getElementById("article").value;
-  if (blog.length  >= 2000 ) {
-      document.getElementById("article").style.border = "5px solid red"; 
-  }
-}
-
-
